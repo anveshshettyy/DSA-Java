75477fb50979f5979b7d8366e26a490ec4c7e661
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-03-26 19:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (19.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -12,17 +12,15 @@ class Solution {
             if(nums[mid] == target) return mid;
 
             if(nums[left] <= nums[mid]) {
-                if(nums[left] <= target && nums[mid] >= target) {
+                if(nums[left] <= target && nums[mid] >= target) 
                     right = mid - 1;
-                } else {
+                else 
                     left = mid + 1;
-                }
             } else {
-                if(nums[right] >= target && nums[mid] <= target) {
+                if(nums[right] >= target && nums[mid] <= target) 
                     left = mid + 1;
-                } else {
+                else 
                     right = mid - 1;
-                }
             }
         }
         return -1;
