6339d9e91b1744332c06f027997594df90c269e7
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-03-30 20:22:42 +0530

    Time: 370 ms (5.15%) | Memory: 45.3 MB (88.15%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,29 +1,20 @@
 class Solution {
-    // static int mid(int nums[], int mid) {
-    //     int sum1 = 0, sum2 = 0;
-    //     for(int i=0; i<mid; i++) 
-    //         sum1 += nums[i];
+    static int mid(int nums[], int mid) {
+        int sum1 = 0, sum2 = 0;
+        for(int i=0; i<mid; i++) 
+            sum1 += nums[i];
 
-    //     for(int i=mid; i<nums.length; i++) 
-    //         sum2 += nums[i];
+        for(int i=mid+1; i<nums.length; i++) 
+            sum2 += nums[i];
         
-    //     return (sum1 == sum2) ? mid : -1;
-        
-    // }
-    public int pivotIndex(int[] arr) {
-        for (int i = 0; i < arr.length; ++i) {
-            int leftSum = 0;
-            for (int j = 0; j < i; j++)
-                leftSum += arr[j];
-
-            int rightSum = 0;
-            for (int j = i + 1; j < arr.length; j++)
-                rightSum += arr[j];
-
-            if (leftSum == rightSum)
+        return (sum1 == sum2) ? mid : -1;
+    }
+    public int pivotIndex(int[] nums) {
+        for(int i=0; i<nums.length; i++) {
+            if(mid(nums, i) != -1) {
                 return i;
+            }
         }
-
         return -1;
     }
 }
