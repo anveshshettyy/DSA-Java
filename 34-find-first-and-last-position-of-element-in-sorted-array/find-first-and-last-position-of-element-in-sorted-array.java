909b296cf15273529c37ea96aaf06191cefc4ed4
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-03-31 11:45:45 +0530

    Time: 1 ms (21.07%) | Memory: 46 MB (67.61%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int right = 0, left = nums.length-1;
+        int left = 0, right = nums.length-1;
         int res[] = {-1,-1};
         if(nums.length == 0) return res;
 
-        // while(left<right) {
-        //     if(nums[left] == target) {
-        //         res[0] = left;
-        //         right--;
-        //     }
-        //     if(nums[right] == target) {
-        //         res[1] = right;
-        //         left++;
-        //     }
-        //     else {
-        //         left++;
-        //         right--;
+        while(left<=right) {
+            if(nums[left] == target && nums[right] == target) {
+                res[0] = left;
+                res[1] = right;
+                return res;
+            }
+
+            if(nums[left] == target) {
+                right--;
+            }
+            else if(nums[right] == target) {
+                left++;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        // int i=0;
+        // for(i=0; i<nums.length; i++) {
+        //     if(nums[i] == target) {
+        //         res[0] = i;
+        //         break;
         //     }
         // }
-        int i=0;
-        for(i=0; i<nums.length; i++) {
-            if(nums[i] == target) {
-                res[0] = i;
-                break;
-            }
-        }
-        for(int j=nums.length-1; j>=i; j--) {
-            if(nums[j] == target) {
-                res[1] = j;
-                break;
-            }
-        }
+        // for(int j=nums.length-1; j>=i; j--) {
+        //     if(nums[j] == target) {
+        //         res[1] = j;
+        //         break;
+        //     }
+        // }
         return res;
     }
 }
