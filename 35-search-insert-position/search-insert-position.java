a926cee1b0c7fa519365b1fd558f7aa7c95610d4
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-03-31 11:16:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (91.97%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,15 +3,12 @@ class Solution {
         int left = 0, right = nums.length-1, pos = 0;
         while(left<=right) {
             int mid = (right + left) / 2;
-            if(nums[mid] == target) {
+            if(nums[mid] == target) 
                 return mid;
-            }
-            if(nums[mid] > target) {
-                right = mid - 1;
-            } else {
+            if(nums[mid] < target) {
                 left = mid + 1;
                 pos = left;
-            }
+            } else right = mid - 1;  
         }
         return pos;
     }
