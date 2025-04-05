a42fcbd7cef5bb87b1da0944dbf137ca3c515521
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-04-05 14:08:41 +0530

    Time: 13 ms (42.89%) | Memory: 45 MB (30.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        char ch[] = s.toCharArray();
+        // s = s.toLowerCase();
+        // char ch[] = s.toCharArray();
 
-        ArrayList <Character> res = new ArrayList<Character>();
-        for(int i=0; i<ch.length; i++) {
-            if((ch[i] >= 97 && ch[i] <= 122) || ( ch[i] >= 48 && ch[i] <= 57))
-                res.add(ch[i]);
-        }
+        // ArrayList <Character> res = new ArrayList<Character>();
+        // for(int i=0; i<ch.length; i++) {
+        //     if((ch[i] >= 97 && ch[i] <= 122) || ( ch[i] >= 48 && ch[i] <= 57))
+        //         res.add(ch[i]);
+        // }
 
-        System.out.println(res);
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0, right = s.length() - 1;
 
-        int left = 0, right = res.size() - 1;
+        // int left = 0, right = res.size() - 1;
         while(left < right) {
-            if(res.get(left) != res.get(right)) {
+            // if(res.get(left) != res.get(right)) {
+                if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
             left++;
