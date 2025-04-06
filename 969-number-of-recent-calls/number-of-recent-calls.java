1955a50814cc2b71f48472e3a1cf65714e5bc6f6
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-04-06 10:44:19 +0530

    Time: 19 ms (90.60%) | Memory: 54.5 MB (34.16%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -3,23 +3,28 @@ class RecentCounter {
     public RecentCounter() {
         queue = new LinkedList<>();
     }
-    
+
     public int ping(int t) {
-        if(queue.size() == 0) {
-            queue.add(t);
-            return 1;
-        } else {
-            if(t <= 3000) {
-                queue.add(t);
-            } else {
-                int margin = t - 3000 ;
-                while(queue.size() != 0 && queue.peek() < margin) {
-                    queue.poll();
-                }
-                queue.add(t);
-            }
-            return queue.size();
+        // if(queue.size() == 0) {
+        //     queue.add(t);
+        //     return 1;
+        // } else {
+        //     if(t <= 3000) {
+        //         queue.add(t);
+        //     } else {
+        //         int margin = t - 3000 ;
+        //         while(queue.size() != 0 && queue.peek() < margin) {
+        //             queue.poll();
+        //         }
+        //         queue.add(t);
+        //     }
+        //     return queue.size();
+        // }
+        queue.add(t);
+        while(queue.peek()  < t - 3000) {
+            queue.poll();
         }
+        return queue.size();
     }
 }
 
