7dcdaec3a6327182450dc19a88c92b53d364141b
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-04-10 11:36:15 +0530

    Time: 4 ms (85.97%) | Memory: 43.4 MB (73.48%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] s = path.split("/");
+
+
+        Stack <String> st = new Stack<String>();
+        for(int i=0; i<s.length; i++) {
+            if(s[i].equals("..")) {
+                if(!st.isEmpty())
+                    st.pop();
+            } else if(s[i].equals(".") || s[i].equals("")) continue;
+             else {
+                st.push(s[i]);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for(String dir : st) {
+            res.append("/").append(dir);
+        }
+        
+        // for(String k:s) System.out.println(k);
+        // System.out.println(res);
+        return (res.length() != 0) ? res.toString() : "/";
+    }
+}
