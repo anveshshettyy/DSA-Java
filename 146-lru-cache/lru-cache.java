cd42a4fa7d23066f3debaf51fcecdcb42e013878
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-04-14 19:24:06 +0530

    Time: 1012 ms (5.00%) | Memory: 130.3 MB (5.60%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,34 @@
+class LRUCache {
+    HashMap <Integer, Integer> map = new HashMap<>();
+    Queue <Integer> queue = new LinkedList<>();
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key)) return -1;
+        queue.remove(key);
+        queue.offer(key);
+        return map.get(key);
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)) {
+            queue.remove(key);
+        } else if(queue.size() == capacity) {
+            int lru = queue.poll();
+            map.remove(lru);
+        }
+        queue.offer(key);
+        map.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
