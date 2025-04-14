cb7212106b3d222e4bc7211a0577d8c4c46ccfda
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-04-14 18:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (77.20%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -1,24 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     static boolean sum(TreeNode root, int target, int sum) {
         if(root == null) return false;
         sum += root.val;
         if(sum == target && root.left == null && root.right == null) return true;
-        Boolean left = sum(root.left, target, sum);
+        boolean left = sum(root.left, target, sum);
         boolean right = sum(root.right, target, sum);
         return (right || left);
     }
