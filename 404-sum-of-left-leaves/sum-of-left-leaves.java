b6a5eda6185423ad7ee2c255171813928e79e35a
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-04-16 14:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (63.61%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -8,32 +8,24 @@
  *     TreeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
- *      -    this.left = left;
+ *         this.left = left;
  *         this.right = right;
  *     }
  * }
  */
 class Solution {
-    // static int sum(TreeNode root, int sum) {
-    //     if(root.left == null && root.right == null) 
-    //         return sum+=root.val;
-    
-    //     int left = sum(root.left,sum);
-    //     int right = sum(root.right, sum);
-    //     return (left + right);
-    // }
-    public int sumOfLeftLeaves(TreeNode root) {
-        if(root==null) return 0;
+    int sum(TreeNode root) {
+        if(root == null) return 0;
 
         int sum = 0;
 
-        if(root.left != null && root.left.left == null && root.left.right == null) {
-            sum += root.left.val;
-        }
-
-        sum += sumOfLeftLeaves(root.left);
-        sum += sumOfLeftLeaves(root.right);
-
+        if(root.left != null && root.left.left == null && root.left.right == null)
+             sum += root.left.val;
+        sum += sum(root.left);
+        sum += sum(root.right);
         return sum;
+    }
+    public int sumOfLeftLeaves(TreeNode root) {
+        return sum(root);
     }
 }
