6555c9ee6592f07c3896279dd68c19a448d90ce3
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-04-24 19:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 51 MB (86.95%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -18,8 +18,8 @@ class Solution {
         if(root==null) return 0;
 
         int sum = 0;
-        if(root.val <= high && root.val >= low) sum += root.val;
         if(root.val > low) sum += rangeSumBST(root.left, low, high);
+        if(root.val <= high && root.val >= low) sum += root.val;
         if(root.val < high) sum += rangeSumBST(root.right, low, high);
         
         return sum;
