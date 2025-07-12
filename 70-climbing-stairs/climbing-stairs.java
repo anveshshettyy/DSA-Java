21e839d1f8345759baa8102d0abafc3d05a4146a
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-12 19:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (27.72%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+        int a = 1, b = 2;
+        for(int i = 2; i<n; i++) {
+            int sum = a + b;
+            a = b; b = sum;
+        }
+        return b;
+    }
+}
