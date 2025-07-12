a766b0d239d150c78b3bc9732ab4a90f444dc047
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-12 20:38:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (24.16%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,16 +1,11 @@
 class Solution {
-    static int MaxLootArray(int arr[], int n) {
-        int dp[] = new int[n+1];
-
-        dp[0] = 0;
-        dp[1] = arr[0];
-        
-        for(int i=2; i<=n; i++) {
-            dp[i] = Math.max((arr[i-1] + dp[i-2]), dp[i-1]);
-        }
-        return dp[n];
-    }
     public int rob(int[] nums) {
-        return MaxLootArray(nums, nums.length);
+        int a = 0, b = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int maxLoot = Math.max((nums[i] + a), b);
+            a = b;
+            b = maxLoot;
+        }
+        return b;
     }
 }
