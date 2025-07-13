926aa5d83a3eb9a96b68f2a3ef47ee49a2645bbc
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-13 21:23:17 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (91.43%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -1,38 +1,23 @@
 class Solution {
-    public int colMax(int nums[][], int i) {
-        int max = 0;
-        for(int j=0; j<nums[0].length; j++) {
-            max = Math.max(nums[i][j], max);
-        }
-        return max;
-    }
-
-    public int rowMax(int nums[][], int j) {
-        int max = 0;
-        for(int i=0; i<nums.length; i++) {
-            max = Math.max(nums[i][j], max);
-        }
-        return max;
-    }
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        int res[][] = new int[grid.length][grid[0].length];
-        int colMax = 0, rowMax = 0;
-        for(int i=0; i<grid.length; i++) {
-            colMax = 0; rowMax = 0;
-            for(int j=0; j<grid[0].length; j++) {
-                colMax = colMax(grid, i);
-                rowMax = rowMax(grid, j);
-                res[i][j] = Math.min(rowMax, colMax);
+        int n = grid.length;
+
+        int[] rowMax = new int[n];
+        int[] colMax = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+                colMax[j] = Math.max(colMax[j], grid[i][j]);
             }
         }
-        
+
         int sum = 0;
-        for(int i=0; i<res.length; i++) {
-            for(int j=0; j<res[0].length; j++) {
-                System.out.print(res[i][j]+" ");
-                sum += res[i][j] - grid[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int allowedHeight = Math.min(rowMax[i], colMax[j]);
+                sum += allowedHeight - grid[i][j];
             }
-            System.out.println();
         }
 
         return sum;
