4ad381ff85cd8f39ec431b0546c0dce8efb661dd
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-13 20:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (30.10%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,21 @@
 class Solution {
-    public void subset(int[] nums,List<Integer> arr, int pos, int n,List<List<Integer>> res) {
+    public List<List<Integer>> list = new ArrayList<>();
+    public void subset(int[] nums, ArrayList<Integer> arr, int pos, int n) {
         if(pos == n) {
-            for(Integer ele : arr){
-                System.out.print(ele + " ");
-            }
-            System.out.println();
-            res.add(new ArrayList<Integer>(arr));
+            list.add(new ArrayList<Integer>(arr));
             return;
         }
-        arr.add(nums[pos]);
-        subset(nums, arr, pos+1, n,res);
-        arr.remove(arr.size()-1);
-        subset(nums, arr, pos+1, n,res);
-    }
 
+        arr.add(nums[pos]);
+        subset(nums, arr, pos+1, n);
+
+        arr.remove(arr.size()-1);
+        subset(nums, arr, pos+1, n);
+    }
+ 
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> arr = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
-        subset(nums, arr, 0, nums.length,res);
-        return res;
+        ArrayList <Integer> arr = new ArrayList<>();
+        subset(nums, arr, 0, nums.length);
+        return list;
     }
 }
