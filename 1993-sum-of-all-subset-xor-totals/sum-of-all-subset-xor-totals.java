9388b1bdf83d855a60bd26cdc47af9af26111e35
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-14 09:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (45.17%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,25 +1,15 @@
 class Solution {
-    public int sum = 0;
-    public void subset(int[] nums, ArrayList<Integer> arr, int pos, int n) {
-        if(pos == n) {
-            int xor = 0;
-            for(int i=0; i<arr.size(); i++) {
-                xor ^= arr.get(i);
-            }
-            sum += xor;
+    int sum = 0;
+    public void subset(int[] nums, int curr, int pos) {
+        if(pos == nums.length) {
+            sum += curr;
             return;
         }
-
-        arr.add(nums[pos]);
-        subset(nums, arr, pos+1, n);
-
-        arr.remove(arr.size()-1);
-        subset(nums, arr, pos+1, n);
+        subset(nums, (curr ^ nums[pos]), pos+1);
+        subset(nums, curr, pos+1);
     }
-
     public int subsetXORSum(int[] nums) {
-        ArrayList <Integer> arr = new ArrayList<>();
-        subset(nums, arr, 0, nums.length);
+        subset(nums, 0, 0);
         return sum;
     }
 }
