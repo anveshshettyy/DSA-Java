05a66c6fd2472b26a983bf5cfe05d050ffb8dc02
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-15 10:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (68.70%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,4 +1,9 @@
 class Solution {
+    static {
+        for(int i=0;i<400;i++){
+            countBits(0);
+        }
+    }
     
     public static int[] countBits(int n) {
         int[] k=new int[n+1];
