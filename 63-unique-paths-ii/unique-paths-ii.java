372fded22b233f2fe160df77933ab74cc281cc26
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-16 12:35:26 +0530

    Time: 1 ms (16.92%) | Memory: 41.9 MB (49.75%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,48 +3,31 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        // if(n==1 || m==1) return 0;
-        if (grid[m - 1][n - 1] == 1)
-            return 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = (grid[i][j] == 0) ? 1 : -1;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                grid[i][j] = grid[i][j] == 0 ? 1 : 0;
             }
         }
 
-        for (int i = m - 2; i >= 0; i--) {
-            if (grid[i][n - 1] == -1 || grid[i + 1][n - 1] == -1) {
-                grid[i][n - 1] = -1;
-            } else {
-                grid[i][n - 1] = grid[i + 1][n - 1];
+        int flag = 1;
+        for(int i=0; i<m; i++) {
+            if(grid[i][0] == 0) flag = 0;
+            grid[i][0] = flag;
+        }
+
+        flag = 1;
+        for(int i=0; i<n; i++) {
+            if(grid[0][i] == 0) flag = 0;
+            grid[0][i] = flag;
+        }
+
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                if(grid[i][j] != 0)
+                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
             }
         }
 
-        for (int j = n - 2; j >= 0; j--) {
-            if (grid[m - 1][j] == -1 || grid[m - 1][j + 1] == -1) {
-                grid[m - 1][j] = -1;
-            } else {
-                grid[m - 1][j] = grid[m - 1][j + 1];
-            }
-        }
-
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                if (grid[i][j] == -1) {
-                    continue;
-                }
-                if (grid[i + 1][j] == -1) {
-                    grid[i][j] = grid[i][j + 1];
-                } else if (grid[i][j + 1] == -1) {
-                    grid[i][j] = grid[i + 1][j];
-                } else {
-                    grid[i][j] = grid[i + 1][j] + grid[i][j + 1];
-                }
-            }
-
-        }
-
-        return (grid[0][0] == -1) ? 0 : grid[0][0];
+        return grid[m-1][n-1];
     }
 }
