13f1e4d5b7d0a2c7d73b62a14a143dc3fb715781
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-21 19:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (51.60%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        res = 0
+        count = 0
+        for i in range(0, len(s)):
+            if s[i] == 'R':
+                count += 1
+            else:
+                count -= 1
+            if count == 0:
+                res += 1
+        return res
+        
