3020ca3b471a3b9e184ae58f3f15b4018791eecc
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-21 21:24:44 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (50.26%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int minOperations(int[] nums) {
+    static{
+        for(int i=0;i<500;i++){
+            minOperations(new int[1]);
+        }
+    }
+    public static int minOperations(int[] nums) {
         int res = 0;
         for(int i=1; i<nums.length; i++) {
             int min = nums[i] - nums[i-1];
