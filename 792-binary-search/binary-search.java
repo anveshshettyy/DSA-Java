fcbc2240835ad1bd05c9bec3e702c37aa7465fd6
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-23 14:31:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (60.45%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,13 +4,12 @@ class Solution {
         while(low <= high) {
             int mid = (low + high) / 2;
             if(nums[mid] == target) return mid;
-            if(nums[mid] > target) {
-                high = mid - 1;
+            if(nums[mid] < target) {
+                low = mid+1;
             } else {
-                low = mid + 1;
+                high = mid - 1;
             }
         }
-
         return -1;
     }
 }
