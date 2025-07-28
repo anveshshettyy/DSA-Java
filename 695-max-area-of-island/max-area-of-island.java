68cebc6dcd6e054d639af505f1f6294d9a1b9e1b
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-28 19:28:07 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (70.41%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,34 +1,31 @@
 class Solution {
-    private int dfs(int[][] grid, int i, int j) {
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
-            return 0;
-        }
-
-        grid[i][j] = 2;
+    public int dfs(int[][] grid, int i, int j, int row, int col) {
+        if(i<0 || i>row-1 || j<0 || j>col-1 || grid[i][j] != 1) return 0;
 
         int count = 1;
+        grid[i][j] = 0;
 
-        count += dfs(grid, i + 1, j);
-        count += dfs(grid, i - 1, j);
-        count += dfs(grid, i, j + 1);
-        count += dfs(grid, i, j - 1);
+        count += dfs(grid, i-1, j, row, col);
+        count += dfs(grid, i+1, j, row, col);
+        count += dfs(grid, i, j-1, row, col);
+        count += dfs(grid, i, j+1, row, col);
 
         return count;
     }
     public int maxAreaOfIsland(int[][] grid) {
-        int count = 0, maxCount = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int row = grid.length; 
+        int col = grid[0].length;
+        int maxCount = 0;
 
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
                 if(grid[i][j] == 1) {
-                    count = dfs(grid, i, j);
+                    int count = dfs(grid, i, j, row, col);
                     maxCount = Math.max(count, maxCount);
                 }
             }
-            
         }
+
         return maxCount;
     }
 }
