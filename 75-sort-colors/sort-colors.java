a3d7a60b73c5e1168c2fb31a3205f1223f6e339f
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-29 10:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (18.00%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,14 +1,26 @@
 class Solution {
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public void sortColors(int[] nums) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        for(int i: nums) {
-            minHeap.add(i);
+        int l = 0, r = nums.length-1;
+        
+        int i = 0;
+        while(i <= r) {
+            if(nums[i] == 0) {
+                swap(nums, i, l);
+                l++; i++; 
+            }
+            else if(nums[i] == 2) {
+                swap(nums, i, r);
+                r--;
+            }
+            else {
+                i++;
+            }
         }
-
-        for(int i=0; i<nums.length; i++) {
-            nums[i] = minHeap.poll();
-        }
-
     }
 }
