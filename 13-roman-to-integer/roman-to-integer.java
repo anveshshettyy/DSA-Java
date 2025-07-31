b69a18f1dd2f14b4816381506ded4ec0d142b749
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-07-31 20:16:08 +0530

    Time: 2 ms (100.00%) | Memory: 44.8 MB (50.48%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -5,47 +5,24 @@ class Solution {
             char c = s.charAt(i);
 
             if(c=='I') sum += 1;
+            
             else if(c=='V'){
-                if(i>0 && s.charAt(i-1) == 'I') {
-                    sum += 3;
-                } else {
-                    sum += 5;
-                }
+                sum += (i>0 && s.charAt(i-1) == 'I') ? 3 : 5;
             } 
             else if(c=='X') {
-                if(i>0 && s.charAt(i-1) == 'I') {
-                    sum += 8;
-                } else {
-                    sum += 10;
-                }
+                sum += (i>0 && s.charAt(i-1) == 'I') ? 8 : 10;
             } 
             else if(c=='L') {
-                if(i>0 && s.charAt(i-1) == 'X') {
-                    sum += 30;
-                } else {
-                    sum += 50;
-                }
+                sum += (i>0 && s.charAt(i-1) == 'X') ? 30 : 50;
             }
             else if(c=='C') {
-                if(i>0 && s.charAt(i-1) == 'X') {
-                    sum += 80;
-                } else {
-                    sum += 100;
-                }
+                sum += (i>0 && s.charAt(i-1) == 'X') ? 80 : 100;
             }
             else if(c=='D') {
-                if(i>0 && s.charAt(i-1) == 'C') {
-                    sum += 300;
-                } else {
-                    sum += 500;
-                }
+                sum += (i>0 && s.charAt(i-1) == 'C') ? 300 : 500;
             } 
             else if(c=='M') {
-                if(i>0 && s.charAt(i-1) == 'C') {
-                    sum += 800;
-                } else {
-                    sum += 1000;
-                }
+                sum += (i>0 && s.charAt(i-1) == 'C') ? 800 : 1000;
             } 
         }
 
