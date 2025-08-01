8e486494bb9f1500cce5eae12d03dae8af2a272b
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-08-01 09:12:42 +0530

    Time: 804 ms (5.24%) | Memory: 45.5 MB (7.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
         int k = s1.length();
 
         char[] ch = s1.toCharArray();
