5d4a7c10199d967e4b74b887898fd1ddb53e6a18
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-08-02 21:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (9.91%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,20 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
-    void helper(int[] nums, int pos, int n, ArrayList<Integer> arr) {
-        if(pos == n) {
-            res.add(new ArrayList<Integer>(arr));
+    void fun(int[] nums, int pos, int end, List<Integer> arr) {
+        if(pos == end) {
+            res.add(new ArrayList<>(arr));
             return;
         }
 
         arr.add(nums[pos]);
-        helper(nums, pos+1, n, arr);
+        fun(nums, pos+1, end, arr);
 
         arr.remove(arr.size()-1);
-        helper(nums, pos+1, n, arr);
+        fun(nums, pos+1, end, arr);
+
     }
     public List<List<Integer>> subsets(int[] nums) {
-        helper(nums, 0, nums.length, new ArrayList<Integer>());
+        fun(nums, 0, nums.length, new ArrayList<>());
         return res;
     }
 }
