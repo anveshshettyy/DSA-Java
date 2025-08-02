4e29f75e59557c3b7b963309b0b4ecbca3a12704
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-08-02 21:52:06 +0530

    Time: 1 ms (92.76%) | Memory: 44.8 MB (29.70%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,27 @@
 class Solution {
-    List <List<Integer>> res = new ArrayList<>();
-    void helper(int[] nums, int i) {
-        if(i == nums.length) {
-            List<Integer> arr = new ArrayList<>();
-            for (int num : nums) arr.add(num);
+    List<List<Integer>> res = new ArrayList<>();
+    void fun(int[] nums, int i) {
+        if(i==nums.length) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int num:nums) arr.add(num);
             res.add(new ArrayList<>(arr));
             return;
         }
 
         for(int j=i; j<nums.length; j++) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-
-            helper(nums, i+1);
-            
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+            swap(i, j, nums);
+            fun(nums, i+1);
+            swap(i, j, nums);
         }
     }
+
+    void swap(int i, int j,int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public List<List<Integer>> permute(int[] nums) {
-        helper(nums, 0);
+        fun(nums, 0);
         return res;
     }
 }
