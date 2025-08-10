d640833ead5607db6ffa0fc4daa1b60ad75bf8a3
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-08-10 21:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (92.05%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,8 +1,13 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(matrix[i][j] == target) return true;
+            int l = 0, r = matrix[0].length-1;
+            while(l<=r) {
+                int mid = (r + l) / 2;
+                int num = matrix[i][mid];
+                if(num == target) return true;
+                else if(num > target) r = mid - 1;
+                else l = mid + 1;
             }
         }
         return false;
