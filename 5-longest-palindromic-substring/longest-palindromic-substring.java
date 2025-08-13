0ba0220108eca6da30bd23f64779605e4c8f98e4
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-08-13 19:09:47 +0530

    Time: 22 ms (42.20%) | Memory: 45.4 MB (26.49%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -5,7 +5,7 @@ class Solution {
 
         for(int i=0; i<s.length(); i++) {
             int l = i, r = i;
-            while(l>=0 && r<s.length() && s.charAt(l) == (s.charAt(r))) {
+            while(l>=0 && r<s.length() && s.charAt(l) == s.charAt(r)) {
                 if((r - l + 1) > resLen) {
                     res = s.substring(l, r+1);
                     resLen = r - l + 1;
@@ -14,7 +14,7 @@ class Solution {
             }
 
             l = i; r = i+1;
-            while(l>=0 && r<s.length() && s.charAt(l) == (s.charAt(r))) {
+            while(l>=0 && r<s.length() && s.charAt(l) == s.charAt(r)) {
                 if((r - l + 1) > resLen) {
                     res = s.substring(l, r+1);
                     resLen = r - l + 1;
