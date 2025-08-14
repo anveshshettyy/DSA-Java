c4b521b419498ea0da27aee68471ed1b63f6692a
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-08-14 21:14:42 +0530

    Time: 6 ms (30.75%) | Memory: 42.6 MB (36.90%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,8 +1,6 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        // int arr[] = new int[10];
-        // ArrayList<Integer> li = new ArrayList<>();
-        PriorityQueue<Integer> li = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         char res = 'a';
         int count = 0;
         for(char c: num.toCharArray()) {
@@ -11,13 +9,12 @@ class Solution {
                 res = c;
                 count = 1;
             }
-            if(count == 3) li.add(c-'0');
+            if(count == 3) maxHeap.add(c-'0');
         }
-        if(li.isEmpty()) return "";
-        int largest = li.poll();
-        StringBuilder result = new StringBuilder();
-
-        for(int i=0; i<3; i++) result.append(largest);
-        return result.toString();
+        if(maxHeap.isEmpty()) return "";
+        int largest = maxHeap.poll();
+        String s = "";
+        s += largest;
+        return s += s.repeat(2);
     }
 }
