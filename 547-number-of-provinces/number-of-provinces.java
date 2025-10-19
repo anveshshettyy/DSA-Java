599772f97bb847130f8a51bc16a9251c8021dfa1
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-10-19 20:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (9.30%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,36 +1,24 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> graph = new ArrayList<>();
         int n = isConnected.length;
-        for(int i=0; i<n; i++) {
-            graph.add(new ArrayList<Integer>());
-        }
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(i!=j && isConnected[i][j] == 1) {
-                    graph.get(i).add(j);
-                }
-            }
-        }
-
-        int vis[] = new int[n];
+        int[] vis = new int[n];
 
         int count = 0;
         for(int i=0; i<n; i++) {
             if(vis[i] == 0) {
                 count++;
-                dfs(graph, i, vis);
+                dfs(isConnected, i, vis);
             }
         }
+
         return count;
     }
-    public void dfs(List<List<Integer>> graph, int node, int[] vis) {
+
+    public void dfs(int[][] graph, int node, int[] vis) {
         vis[node] = 1;
-        for(int i=0; i< graph.get(node).size(); i++) {
-            int neighbour = graph.get(node).get(i);
-            if(vis[neighbour] == 0) {
-                dfs(graph, neighbour, vis);
+        for(int i=0; i<graph[0].length; i++) {
+            if(vis[i] == 0 && graph[node][i] == 1) {
+                dfs(graph, i, vis);
             }
         }
     }
