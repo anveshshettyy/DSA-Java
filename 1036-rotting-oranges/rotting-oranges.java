d5a1031a4319166fb1e39585c756655fa26f64b0
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-10-20 22:05:16 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (49.98%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,42 +1,44 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int r = grid.length, c = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        int count = 0;
+        int row = grid.length;
+        int col = grid[0].length;
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+        Queue<int[]> q = new LinkedList<>();
+
+        int cntOnes = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 if (grid[i][j] == 2) {
                     q.add(new int[] { i, j });
                 } else if (grid[i][j] == 1) {
-                    count++;
+                    cntOnes++;
                 }
             }
         }
 
-        if (count == 0)
-            return 0;
-        int countMin = 0, cnt = -1 * q.size();
-        int dx[] = { 0, 0, 1, -1 };
-        int dy[] = { 1, -1, 0, 0 };
+        int[] dx = { 1, -1, 0, 0 };
+        int[] dy = { 0, 0, 1, -1 };
+
+        int time = 0;
         while (!q.isEmpty()) {
             int size = q.size();
-            cnt += size;
-            for (int i = 0; i < size; i++) {
-                int point[] = q.remove();
-                for (int j = 0; j < 4; j++) {
-                    int x = point[0] + dx[j];
-                    int y = point[1] + dy[j];
-                    if (x < 0 || y < 0 || x >= r || y >= c || grid[x][y] == 0 || grid[x][y] == 2) {
-                        continue;
+            boolean newrot = false;
+            for (int s = 0; s < size; s++) {
+                int[] idx = q.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nrow = idx[0] + dx[i];
+                    int ncol = idx[1] + dy[i];
+                    if (nrow >= 0 && nrow < row && ncol >= 0 && ncol < col && grid[nrow][ncol] == 1) {
+                        q.add(new int[] { nrow, ncol });
+                        grid[nrow][ncol] = 2;
+                        newrot = true;
+                        cntOnes--;
                     }
-                    grid[x][y] = 2;
-                    q.add(new int[] { x, y });
                 }
             }
-            if (!q.isEmpty())
-                countMin++;
+            if (newrot) time++;
         }
-        return cnt == count ? countMin : -1;
+
+        return (cntOnes == 0) ? time : -1;
     }
 }
